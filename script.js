dca8051e350f7850535e7df16bc6f08d7cf8f173
George Lynch <osu4life75@gmail.com>
2024-04-01 21:28:13 -0500

    Descriptive Sections/Fixed Lazy loading
    Adding Experience and Education.
    Fixed Lazy Loading in Certification loading.

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+//lazy loading in Certification section
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lazyImages = document.querySelectorAll(".certification");
+
+    const lazyLoad = function() {
+        lazyImages.forEach(function(img) {
+            if (img.getBoundingClientRect().top <= window.innerHeight && img.getBoundingClientRect().bottom >= 0 && getComputedStyle(img).display !== "none") {
+                img.src = img.dataset.src;
+                img.onload = function() {
+                    img.removeAttribute("data-src");
+                };
+            }
+        });
+    };
+
+    document.addEventListener("scroll", lazyLoad);
+    window.addEventListener("resize", lazyLoad);
+    window.addEventListener("orientationchange", lazyLoad);
+});
+
+// Get the current year in footer
+const currentYear = new Date().getFullYear();
+
+// Update the text content of the currentYear span element
+document.getElementById("currentYear").textContent = currentYear;
+
+
